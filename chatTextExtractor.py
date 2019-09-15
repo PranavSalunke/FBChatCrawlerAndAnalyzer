f8ef105b41e9576b71767900cae51aa3fb8c15a4
Pranav Salunke <pranav.salunke@gmail.com>
2019-09-14 17:15:04 -0700

    now has an option to create a csv file as well

--- chatTextExtractor.py
+++ chatTextExtractor.py
@@ -1,10 +1,12 @@
+import csv
 import json
 import datetime
 
 
-def extract(jsonfilename, outputfilename, printToConsole):
+def extract(jsonfilename, outputfilename, csvfilename, printToConsole):
     with open(jsonfilename, "r") as chatdatafile, open(outputfilename, "w") as justtext:
 
+        csvrows = []
         chatdata = json.load(chatdatafile)
         messages = chatdata["messages"]
         authors = chatdata["authors"]
@@ -37,9 +39,23 @@ def extract(jsonfilename, outputfilename, printToConsole):
                 body = "<<UnicodeEncodeError on this message. Can happen because of special characters and emojis. Message ID: %s>>" % (m["uid"])
                 justtext.write("[%s] %s  %s (%s)\n%s\n\n" % (messageNumberStr, author, timestamp, humanreadabletime, body))
 
+            if csvfilename:
+                # header: "AuthorID", "AuthorName", "Message", "Timestamp"
+                body = body.replace("\n", "<<\\n>>")
+                csvrows.append([m["author"], author, body, timestamp])
+
+        # make csv file
+        if csvfilename:
+            with open(csvfilename, "w", newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                header = ["AuthorID", "AuthorName", "Message", "Timestamp"]
+                csvwriter.writerow(header)
+                csvwriter.writerows(csvrows)
+
 
 # SETTINGS
 printToConsole = False  # goes faster if this is False
 jsonfilename = "chatdata.json"
 outputfilename = "messagetext.txt"
-extract(jsonfilename, outputfilename, printToConsole)
+csvfilename = "messagetext.csv"  # put None for no csv
+extract(jsonfilename, outputfilename, csvfilename, printToConsole)

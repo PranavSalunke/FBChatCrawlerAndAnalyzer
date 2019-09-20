6702a747c2ba02bf3a2aa9cec4932deac4a67bbb
Pranav Salunke <pranav.salunke@gmail.com>
2019-09-19 21:47:11 -0700

    graphs by author now as well. some cleanup

--- plotMessages.py
+++ plotMessages.py
@@ -3,6 +3,7 @@ import csv
 import pandas as pd
 import datetime
 from bokeh.plotting import figure, output_file, show, save
+from bokeh.palettes import Category10, Plasma256
 import matplotlib.pyplot as plt
 import time
 
@@ -40,6 +41,78 @@ def makeAggregateDF(df, timestampCol, countCol, freq):
     return msgCountsDF.resample(freq).sum()
 
 
+def getFreqName(freq):
+    name = freq  # returns freq if unknown
+    # remove digits if present
+    clean = "".join([i for i in freq if not i.isdigit()]).strip()
+    # S - second; T- minute; H - hour; D - day; W - week; M - month; A - year
+
+    if clean == "S":
+        name = "second(s)"
+    elif clean == "T":
+        name = "minute(s)"
+    elif clean == "H":
+        name = "hour(s)"
+    elif clean == "D":
+        name = "day(s)"
+    elif clean == "W":
+        name = "week(s)"
+    elif clean == "M":
+        name = "month(s)"
+    elif clean == "A":
+        name = "year(s)"
+
+    return name
+
+
+def graphTotalOnlyLine(chatmessageDF, timestampCol, freq):
+    countCol = "Count"
+    msgCountsDF = makeAggregateDF(chatmessageDF, timestampCol, countCol, freq)
+
+    # graph it
+    # note you will have to remove the html files yourself
+
+    freqname = getFreqName(freq)
+    t = "%s grouped by %s vs counts for all messages" % (timestampCol, freqname)
+    plot = figure(x_axis_type='datetime', plot_width=800, plot_height=500, title=t)
+    plot.xaxis.axis_label = "%s time" % (timestampCol)
+    plot.yaxis.axis_label = "Count"
+    plot.line(x=timestampCol, y=countCol, source=msgCountsDF, line_width=2, color="black")
+    output_file("plotMessages_Total.html")
+    show(plot)
+
+
+def graphAuthorsLine(chatmessageDF, timestampCol, freq, graphTotal=False):
+    uniqueAuthors = list(chatmessageDF["AuthorName"].unique())
+    authorCountColNames = []
+    # make counts per author on the row (1 if that author, 0 if not)
+    for author in uniqueAuthors:
+        colName = "".join([i.lower().capitalize() for i in author.split()])+"Count"
+        authorCountColNames.append(colName)
+        chatmessageDF[colName] = chatmessageDF["AuthorName"].apply(lambda a: 1 if a == author else 0)
+        # print(colName)
+
+    # put all authors on one graph
+    freqname = getFreqName(freq)
+    t = "%s grouped by %s vs counts by author" % (timestampCol, freqname)
+    plot = figure(x_axis_type='datetime', plot_width=800, plot_height=500, title=t)
+    plot.xaxis.axis_label = "%s time" % (timestampCol)
+    plot.yaxis.axis_label = "Count by Author"
+    if graphTotal:
+        msgCountsDF = makeAggregateDF(chatmessageDF, timestampCol, "Count", freq)
+        plot.line(x=msgCountsDF.index, y=msgCountsDF["Count"], line_width=2, alpha=0.2, color="black", legend="Total")
+
+    colorsToUse = Category10[10] if len(uniqueAuthors) <= 10 else Plasma256
+    for countCol, color in zip(authorCountColNames, colorsToUse):
+        msgCountsDF = makeAggregateDF(chatmessageDF, timestampCol, countCol, freq)
+        plot.line(x=msgCountsDF.index, y=msgCountsDF[countCol], line_width=2, alpha=0.6, color=color, legend=countCol)
+
+    plot.legend.location = "top_left"
+    plot.legend.click_policy = "hide"
+    output_file("plotMessages_Authors.html")
+    show(plot)
+
+
 def makeTimelinePlotCSV(csvfilename):
     with open(csvfilename, "r") as chatmessagescsv:
         chatmessageDF = pd.read_csv(chatmessagescsv)
@@ -49,30 +122,19 @@ def makeTimelinePlotCSV(csvfilename):
         # chatmessageDF["TimestampPST"] = chatmessageDF["TimestampUTC"].dt.tz_localize(tz="PST8PDT")  # dont think this actually works
         # add a column for the count of messages per row (just 1)
         numRows = len(chatmessageDF.index)
-        counts = [1]*numRows
-        chatmessageDF["Count"] = counts
+        chatmessageDF["Count"] = [1]*numRows  # column of 1s
 
         # aggregate by time. need to make new data frame
 
-        freq = "H"  # list of times: https://stackoverflow.com/a/17001474
+        freq = "T"  # list of times: https://stackoverflow.com/a/17001474
         # S - second; T- minute; H - hour; D - day; W - week; M - month; A - year; can do 5T for 5 minutes
 
         # total counts
         timestampCol = "TimestampUTC"
-        countCol = "Count"
-        msgCountsDF = makeAggregateDF(chatmessageDF, timestampCol, countCol, freq)
-        # print(msgCountsDF)
-
-        # graph it
-        t = "%s grouped by %s vs counts for all messages" % (timestampCol, freq)
-        plot = figure(x_axis_type='datetime', plot_width=800, plot_height=500, title=t)
-        plot.xaxis.axis_label = "%s time" % (timestampCol)
-        plot.yaxis.axis_label = "Count"
-        plot.line(x=timestampCol, y=countCol, source=msgCountsDF, line_width=3)
-        show(plot)
-        # note you will have to remove the html files yourself
+        graphTotalOnlyLine(chatmessageDF, timestampCol, freq)
 
         # counts by author
+        graphAuthorsLine(chatmessageDF, timestampCol, freq, graphTotal=True)
 
 
 makeTimelinePlotCSV("messagetext_100.csv")
